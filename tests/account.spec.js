a0fdb6c365680273c7dca161385ec24028754d4e
Jaeger George Richard Statton <54759035+JaegerGRS@users.noreply.github.com>
2026-02-26 17:07:04 +0000

    a

--- tests/account.spec.js
+++ tests/account.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 test('Account dashboard username change', async ({ page }) => {
   await page.goto('http://localhost:8000/login.html');
